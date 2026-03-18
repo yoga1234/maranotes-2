69d2e161314ac18b92f974962fdb083a12124f50
yoga1234 <septianayoga30@gmail.com>
2026-03-18 13:58:51 +0700

    fixing styling on the page

--- src/module/leftSide/noteButtonContainer.js
+++ src/module/leftSide/noteButtonContainer.js
@@ -1,7 +1,7 @@
 export function projectNoteButton() {
   return `
     <div class=project-note-button>
-      <button class="note-open-btn">Note Open</button>
+      <button class="note-open-btn active">Note Open</button>
       <button class="note-done-btn">Note Done</button>
     </div>
   `;
